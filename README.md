921926207dd92d795799bf500066a211bc9de8ef
Filip Kalkan <filip_kalkan@hotmail.se>
2023-01-30 16:06:15 +0100

    Added main

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Source code for the masters thesis "Finding Candidate Node Pairs for Link Predic
 ## About
 This repository includes a benchmarking framework for testing candidate selection algorithms, along with implementations of some candidate selection algorithms, one of them being DAPPR.
 
-A number of of datasets of varying domains and sizes are available for testing. Tools for vizualizing the results are available in `src/graphs.ipynb`.
+A number of of datasets of varying domains and sizes are available for testing.
 
 ## Setup
 The project requires Python 3.10.
@@ -12,4 +12,15 @@ The project requires Python 3.10.
 Using conda, install all dependencies in `environment.yml`.
 
 ## Quick Start
+### Compare Algorithms
 Once dependencies are installed, you can try running the benchmarks in `multi.ipynb`.
+
+Tools for vizualizing the results are available in `src/graphs.ipynb`.
+### Run DAPPR
+DAPPR can be run in `src/main.py`. Example:
+
+```
+python main.py --edgelist-path "dataset/static/yeast.txt" --k 1000 --parallel y -out candidate_node_pairs.csv
+```
+
+This example runs the `YEAST` dataset and outputs the candidate node pairs to a csv file named `candidate_node_pairs.csv`.
---
+++ src/main.py
@@ -0,0 +1,42 @@
+from candidate_selection.dappr.dappr import Dappr
+import argparse
+import os
+import networkx as nx
+
+def parse_args():
+    def str2bool(v):
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+    
+    parser = argparse.ArgumentParser(description='Run DAPPR to extract candidate node pairs.')
+    parser.add_argument('--edgelist-path', type=str, required=True, help="Relative path to edgelist representing the graph.")
+    parser.add_argument('--k', '-k', type=int, required=True, help="Number of candidate node pairs to generate per node.")
+    parser.add_argument('--alpha', '-a', type=float, required=False, default=0.8, help="Non-restart probability.")
+    parser.add_argument('--epsilon', '-e', type=float, required=False, default=0.05, help="Error threshold.")
+    parser.add_argument('--parallel', '-p', type=str2bool, required=False, default=True, help="Run parallel processes.")
+    parser.add_argument('--verbose', '-v', type=str2bool, required=False, default=False, help="Verbose.")
+    parser.add_argument('--bipartite', '-b', type=str2bool, required=False, default=False, help="Whether the evaluated graph is bipartite or not.")
+    parser.add_argument('--out', '-o', type=str, required=True, help="Relative path to output file.")
+    return parser.parse_args()
+
+def main(args):
+    cwd = os.getcwd()
+    edgelist_path = os.path.join(cwd, args.edgelist_path)
+    G = nx.read_edgelist(edgelist_path)
+    dappr = Dappr(G, args.k, args.epsilon, args.alpha, args.parallel, args.verbose, args.bipartite)
+    candidate_node_pairs = dappr.run()
+    output_path = os.path.join(cwd, args.out)
+    if '.' not in output_path:
+        output_path = output_path + '.csv'
+    with open(output_path, 'w') as f:
+        for candidate_node_pair in candidate_node_pairs:
+            f.write(f"{str(candidate_node_pair[0])},{str(candidate_node_pair[1])}\n")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
